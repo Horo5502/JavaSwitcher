3d4bf124ffefd5e97e9e44756a18da5d587678b2
Horo5502 <95289494+Horo5502@users.noreply.github.com>
2023-09-23 15:45:50 +0900

    Update swjava.ps1

--- sub/swjava.ps1
+++ sub/swjava.ps1
@@ -1,2 +1,4 @@
 Start-Process -FilePath "powershell" $PSScriptRoot\JavaSwitcher\JavaSwitcher.ps1 -Wait
+# デフォルトでwindowsに入っているpowershell5.1には-UseNewEnvironment(環境変数再読み込み)のオプションがないのでコメントアウト
+# Microsoftのgithubのreleaseにある最新版のpwshならこのオプションは存在する
 # Start-Process cmd -UseNewEnvironment
